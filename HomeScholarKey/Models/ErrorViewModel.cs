94b3556adb680470eef98d138be4d1b7f6d71efe
duduzilejan <duduzilejan09@gmail.com>
2022-01-24 10:23:14 +0200

    Deleted unneccessary templates files

--- HomeScholarKey/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HomeScholarKey.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
